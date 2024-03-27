bbc8d365433741a13ec91fb788432da0ad3e1bea
Sondre Kulseng <sondre.kulseng@gmail.com>
2024-03-27 12:23:48 +0100

    Redirect if user is not logged in

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { auth } from "/firebase.config"
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useState } from 'react'
 import { Skeleton } from '@mantine/core'
+import { useRouter } from 'next/navigation'
 import SignOutButton from './signOutButton'
+import { auth } from "/firebase.config"
 
 export default function Page() {
 	const [user, loading, error] = useAuthState(auth)
+	const router = useRouter()
 
 	if (loading) {
 		return <Skeleton height={8} mt={6} height={30} width="50%" />
@@ -15,6 +17,10 @@ export default function Page() {
 
 	if (error) {
 		return <p>Error occured</p>
+	}
+
+	if (!user) {
+		router.push("/login")
 	}
 
 	if (user) {
