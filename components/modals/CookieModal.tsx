81da90c4acc5332beccca6cbf1b4f1ca7adc926b
Sondre Kulseng <sondre.kulseng@gmail.com>
2024-11-03 18:43:19 +0100

    Bug fix

--- components/modals/CookieModal.tsx
+++ components/modals/CookieModal.tsx
@@ -21,7 +21,7 @@ export default function CookieModal({ open, setOpen }: Readonly<Props>) {
         <Modal opened={open} onClose={cookiesDeclined} title="Cookie consent" size="lg" overlayProps={{ backgroundOpacity: 0.55, blur: 3 }}>
             <h3>Cookie consent</h3>
             <p>This website uses cookies for functional and analytical purposes.<br />
-                By clicking "Accept", you consent to the use of cookies.
+                By clicking &apos;Accept&apos;, you consent to the use of cookies.
             </p>
             <Button onClick={cookiesAccepted} color="blue">Accept</Button> |
             <Button onClick={cookiesDeclined} color="#5e5e5e">Decline (closes site)</Button>
