38efaff05c57435e6025532de9db28eb81673e37
Sondre Kulseng <55538572+sondrekulseng@users.noreply.github.com>
2024-12-09 12:29:13 +0100

    Update DeleteAccountModal.tsx

--- components/modals/DeleteAccountModal.tsx
+++ components/modals/DeleteAccountModal.tsx
@@ -11,7 +11,7 @@ export default function DeleteAccountModal({ open, setOpen }: Readonly<Props>) {
 
     return (
         <Modal opened={open} onClose={() => setOpen(false)} title="Delete account" size="lg" overlayProps={{ backgroundOpacity: 0.55, blur: 3 }}>
-            <h3>Delete your account and related  data?</h3>
+            <h3>Delete your account and related data?</h3>
             <p>This action cannot be undone</p>
             <DeleteAccountButton />
             <Button onClick={() => setOpen(false)} style={{marginLeft: '1em'}} variant="default">Cancel</Button>
